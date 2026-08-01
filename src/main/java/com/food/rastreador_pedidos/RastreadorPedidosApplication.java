0609d1669f9c73c003ef1e29e4ec62d06da32b8f
gabrielVrodriguez <121834753+gabrielVrodriguez@users.noreply.github.com>
2026-08-01 18:38:47 -0300

    setup inicial do projeto Spring Boot

---
+++ src/main/java/com/food/rastreador_pedidos/RastreadorPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.food.rastreador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreadorPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/rastreador_pedidos/RastreadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.rastreador_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
